9af163b9d28d9110783f9a902eb98839b5e688d6
FlashBurn-inc <vitaliy@deminov.net>
2018-04-01 17:01:31 +0300

    upload to github

---
+++ task4_3.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+# check parametrs
+if [[ $# != 2 ]]
+then
+	echo "ERROR: For launch script need 2 parameters" 1>&2
+	echo "1 - path to folder for which you need a backup" 1>&2
+	echo "2 - quantity backup files" 1>&2
+	exit 1
+fi
+
+src_dir=$1
+num_backup=$2
+
+#check backup dir
+dst_dir="/tmp/backups/"
+[ -d "/tmp" ] || mkdir /tmp
+[ -d $dst_dir ] || mkdir /tmp/backups
+
+#date form
+date=$(date +'%F-%H%M%S')
+
+#check first parametr
+if [[ $src_dir != /* ]]
+then
+        echo "ERROR: Wrong path" 1>&2
+        exit 1
+fi
+
+#check second parametr
+if [[ -n $(echo $num_backup | grep -v '^[0-9][0-9]*$') ]];
+then
+        echo "$num_backup - bad parameter!" 1>&2
+        exit 1
+fi
+
+#check directory or file for exist 
+if [[ -d "$src_dir" || -f "$src_dir" ]]
+then
+#form file name and tar
+	name=$(echo "$src_dir" | sed -r 's/^\/*//g' | sed 's/\/\/*/-/g')
+#	echo "$name"
+	dst_name=$(echo "$name"-"$date".tar.gz)
+#	echo $dst_dir"_"$dst_name"_"$src_dir
+	tar -czf "$dst_dir""$dst_name" "$src_dir" 2>/dev/null
+else
+	echo "ERROR: No such file or directory" 1>&2
+	exit 1
+fi
+#remove old file
+rm -f $(ls -sl $(echo "$dst_dir""$name""*") | head -n -"$num_backup" | awk '{print $10}')
